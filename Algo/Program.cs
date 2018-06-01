ad7059810c4d8731a008900a1cf5a7ef12c94998
nbl241 <abilaz24@gmail.com>
2018-06-01 17:05:48 +0200

    Rajout de sous menu

--- Algo/Program.cs
+++ Algo/Program.cs
@@ -13,14 +13,97 @@ namespace Algo
         {
             {
                 Menu menu = new Menu();
-                menu.InsererLigne(new MenuItem { Numero = 1, Libelle = "menu 1" });
-                menu.InsererLigne(new MenuItem { Numero = 3, Libelle = "menu 3" });
-                menu.InsererLigne(new MenuItem { Numero = 3, Libelle = "menu 3BIS" });
-                menu.InsererLigne(new MenuItem { Numero = 4, Libelle = "menu 4" });
+                menu.InsererLigne(new MenuItem { Numero = 1, Libelle = "Voyages" });
+                menu.InsererLigne(new MenuItem { Numero = 2, Libelle = "Clients" });
+                menu.InsererLigne(new MenuItem { Numero = 3, Libelle = "Dossiers" });
 
+                menu.Afficher();
                 int resultat = menu.Choisir();
+                switch (resultat)
+                {
+                    case 1:
+                        SousMenuVoyages();
+                        break;
+                    case 2:
+                        SousMenuClients();
+                        break;
+                    case 3:
+                        SousMenuDossier();
+                        break;
+                }
+                Console.ReadLine();
+
+
+
                 Console.WriteLine(resultat);
                 Console.ReadLine();
+            }
+        }
+
+        private static void SousMenuDossier()
+        {
+            Menu sousMenu = new Menu();
+            sousMenu.InsererLigne(new MenuItem { Numero = 1, Libelle = "Creer" });
+            sousMenu.InsererLigne(new MenuItem { Numero = 2, Libelle = "Rechercher" });
+            sousMenu.InsererLigne(new MenuItem { Numero = 3, Libelle = "Supprimer" });
+
+            sousMenu.Afficher();
+            int resultatSousMenu = sousMenu.Choisir();
+            switch (resultatSousMenu)
+            {
+                case 1:
+                    Console.WriteLine("Vous souhaiter : Dossier => Creer");
+                    break;
+                case 2:
+                    Console.WriteLine("Vous souhaiter : Dossier => Rechercher");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Vous souhaiter : Dossier => Supprimer");
+                    break;
+            }
+        }
+
+        private static void SousMenuClients()
+        {
+            Menu sousMenu = new Menu();
+            sousMenu.InsererLigne(new MenuItem { Numero = 1, Libelle = "Creer" });
+            sousMenu.InsererLigne(new MenuItem { Numero = 2, Libelle = "Rechercher" });
+            sousMenu.InsererLigne(new MenuItem { Numero = 3, Libelle = "Supprimer" });
+
+            sousMenu.Afficher();
+            int resultatSousMenu = sousMenu.Choisir();
+            switch (resultatSousMenu)
+            {
+                case 1:
+                    Console.WriteLine("Vous souhaiter : Clients => Creer");
+                    break;
+                case 2:
+                    Console.WriteLine("Vous souhaiter : Clients => Rechercher");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Vous souhaiter : Clients => Supprimer");
+                    break;
+            }
+        }
+
+        private static void SousMenuVoyages()
+        {
+            Menu sousMenu = new Menu();
+            sousMenu.InsererLigne(new MenuItem { Numero = 1, Libelle = "Destination" });
+            sousMenu.InsererLigne(new MenuItem { Numero = 2, Libelle = "Compagnies" });
+
+            sousMenu.Afficher();
+            int resultatSousMenu = sousMenu.Choisir();
+            switch (resultatSousMenu)
+            {
+                case 1:
+                    Console.WriteLine("Vous souhaiter : voyages => destination");
+                    break;
+                case 2:
+                    Console.WriteLine("Vous souhaiter : voyages => compagnies");
+                    break;
             }
         }
     }
